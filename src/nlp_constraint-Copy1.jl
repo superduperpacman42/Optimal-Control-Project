5af78ca2964c5957027e13c2e65fd770dff8e23d
Alex Stutt <alexandra.stutt@gmail.com>
2022-05-10 14:07:37 -0400

    simple ref traj, not rly helping

---
+++ src/nlp_constraint-Copy1.jl
@@ -0,0 +1,99 @@
+"""
+    eval_c!(nlp, c, Z)
+
+Evaluate all the constraints
+"""
+function eval_c!(nlp::HybridNLP, c, Z)
+    xi = nlp.xinds
+    c[nlp.cinds[1]] .= Z[xi[1]] - nlp.x0
+    c[nlp.cinds[2]] .= Z[xi[end]] - nlp.xf
+    dynamics_constraint!(nlp, c, Z)
+    length_constraint!(nlp, c, Z)
+    height_constraint!(nlp, c, Z)
+    return c
+end
+
+function dynamics_constraint!(nlp::HybridNLP, c, Z)
+    
+end
+
+function length_constraint!(nlp::HybridNLP, c, Z)
+    # Create a view for the portion for the length constraints
+    d = view(c, nlp.cinds[4])
+    
+    # Some useful variables
+    xi,ui = nlp.xinds, nlp.uinds
+    N = nlp.N                      # number of time steps
+    M = nlp.M                      # time steps per mode
+    Nmodes = nlp.Nmodes            # number of mode sequences (N ÷ M)
+    
+    # TODO: Calculate the length constraints
+    
+    # grab X and U from Z
+    X,U = unpackZ(nlp, Z)
+    
+    for k = 1:N # 2 lengths constraints per timestep, 1 for each foot
+        d[2*k - 1] = norm(X[k][1:2] - X[k][3:4]) # foot 1
+        d[2*k] = norm(X[k][1:2] - X[k][5:6]) # foot 2
+    end
+end
+
+function height_constraint!(nlp::HybridNLP, c, Z)
+
+end
+
+"""
+    jac_c!(nlp, jac, Z)
+
+Evaluate the constraint Jacobians.
+"""
+function jac_c!(nlp::HybridNLP{n,m}, jacvec::AbstractVector, Z) where {n,m}
+    jac = NonzerosVector(jacvec, nlp.blocks)
+    jacvec .= 0
+    
+    xi,ui = nlp.xinds, nlp.uinds
+    model = nlp.model
+    N = nlp.N                      # number of time steps
+    M = nlp.M                      # time steps per mode
+    X, U = unpackZ(nlp, Z)
+    modes = nlp.modes
+    c_init_inds, c_term_inds, c_dyn_inds, c_length_inds, c_height_inds = nlp.cinds
+    
+    # Init/final n, n
+    jac[c_init_inds, xi[1]] = I(n)
+    jac[c_term_inds, xi[end]] = I(n)
+    
+    # Dynamics n(N-1)
+    for k = 1:N-1
+        t = nlp.times[k]
+        dt = nlp.times[k+1] - t
+        A, B = discrete_jacobian(model, X[k], U[k], t, dt, modes[:,k])
+        if any(modes[:,k] .!= modes[:,k+1])
+            J = jump_jacobian(modes[:,k+1])
+            jac[c_dyn_inds[(k-1)*n+1:k*n], vcat(xi[k], ui[k], xi[k+1])] = hcat(J*A, J*B, -LinearAlgebra.I(n))
+        else
+            jac[c_dyn_inds[(k-1)*n+1:k*n], vcat(xi[k], ui[k], xi[k+1])] = hcat(A, B, -LinearAlgebra.I(n))
+        end
+    end
+    
+    # Length 4N
+    s = [model.s1, model.s2, model.s3, model.s4]
+    for k = 1:N
+        v = zeros(4,n)
+        Q = q2Q(X[k][4:7])
+        for l = 1:4
+            L = X[k][1:3] + Q*s[l] - X[k][3*l+5:3*l+7]
+            v[l,1:3] = 2*L
+            v[l,3*l+5:3*l+7] = -2*L
+            # TODO: derivative with respect to orientation, v[l,4:7] = 2*L*?
+        end
+        jac[c_length_inds[k*4-3:k*4], xi[k]] = v
+    end
+    
+    # Height 4N
+    for k = 1:N
+        for l = 1:4
+            jac[c_height_inds[k*4+l-4],xi[k][3*l+10]] = 1
+        end
+    end
+end
--- src/ref_trajectory.jl
+++ src/ref_trajectory.jl
@@ -1,40 +1,45 @@
-function reference_trajectory(model::SimpleQuadruped, times, modes, terrain::Terrain;
+function reference_trajectory(model::SimpleQuadruped, times, modes, step_length, terrain::Terrain;
         xinit = 0.0,
         xterm = 10.0
     )
-    
-    height = 0.5*model.ℓmax # just guessed on this
+    # no global planning, just walking in a straight line right now
     
     # Some useful variables
     n,m = size(model)
     tf = times[end]
     N = length(times)
-    Δx = xterm - xinit
     
     mb,g = model.mb, model.g
     body_width = 2*abs(model.s1[2]) # from FL shoulder y pos wrt COM
     body_length = 2*abs(model.s1[1]) # from FL shoulder x pos wrt COM
+    nft = size(modes)[1] # nft lol
+    
+    body_height = 0.7*model.ℓmax # const body height
+    ft_height = 0.25*model.ℓmax # swing foot height
+    
+    nsteps = sum(modes, dims=2) # number of steps each foot takes 
+    dx = (xterm - xinit) ./ nsteps # dx average dist moved for each timestep in swing
+    dt_ft = tf ./ nsteps
+    
+    xs = range(xinit,xterm,length=N) # body xpos
+    dt = tf/N
     
     # initialization
     xref = zeros(n,N)
     uref = zeros(m,N)
     
-    # linearly interpolate x-pos
-    # constant y-pos
-    # velocities by taking difference
+    # add/subtract half body length, for front vs back feet
+    xref[8,1] = (body_length/2)
+    xref[11,1] = -(body_length/2)
+    xref[14,1] = (body_length/2)
+    xref[17,1] = -(body_length/2)
     
-    xs = range(xinit,xterm,length=N)
-    
-    dt = tf/Δx
-    
-    # do this smarter in the future
-    # to make a less boring trajectory
     
     for k = 1:N-1
           
         xref[1,k] = xs[k] # body x
         xref[2,k] = 0 # body y
-        xref[3,k] = height # body z
+        xref[3,k] = body_height # body z
         
         # no body rotation in reference
         xref[4,k] = 1 # no rotation
@@ -42,25 +47,50 @@ function reference_trajectory(model::SimpleQuadruped, times, modes, terrain::Ter
         # foot index: (1,2,3,4) = (FL,BL,FR,BR)
         
         # foot x pos
-        xref[8,k] = xs[k] + (body_length/2)
-        xref[11,k] = xs[k] - (body_length/2)
-        xref[14,k] = xs[k] + (body_length/2)
-        xref[17,k] = xs[k] - (body_length/2)
+        # pick up foot when in swing
+        xinds = (8,11,14,17)
+        for i = 1:nft # num feet
+            if modes[i,k] == 0 # in swing
+               xref[xinds[i],k+1] = xref[xinds[i],k] + dx[i]
+            else
+               xref[xinds[i],k+1] = xref[xinds[i],k]
+            end
+        end
+        # would make more sense to lin interp over each swing phase
+        # while checking for final swing phase, must ensure you reach xterm in final swing phase
+        
         
         # foot y pos
+        # keep const
         xref[9,k] = body_width / 2
         xref[12,k] = body_width / 2
         xref[15,k] = -body_width / 2
         xref[18,k] = -body_width / 2
         
+        
         # foot z pos
-        # keep at 0
+        # pick up foot when in swing
+        zinds = (10,13,16,19)
+        for i = 1:nft # num feet
+            if modes[i,k] == 0 # in swing
+               xref[zinds[i],k] = ft_height 
+            end
+        end
+        
+        # set velocities
+        # keep at 0 for k = 1
         if k > 1
-            xref[20,k] = (xs[k+1] - xs[k])/dt # body x vel
-            xref[26,k] = (xs[k+1] - xs[k])/dt # foot1 x vel
-            xref[29,k] = (xs[k+1] - xs[k])/dt # foot2 x vel
-            xref[32,k] = (xs[k+1] - xs[k])/dt # foot3 x vel
-            xref[35,k] = (xs[k+1] - xs[k])/dt # foot4 x vel
+            xref[20,k] = (xref[1,k+1] - xref[1,k])/dt # body x vel
+            
+            xref[26,k] = (xref[8,k+1] - xref[8,k])/dt_ft[1] # foot1 x vel
+            xref[29,k] = (xref[11,k+1] - xref[11,k])/dt_ft[2] # foot2 x vel
+            xref[32,k] = (xref[14,k+1] - xref[14,k])/dt_ft[3] # foot3 x vel
+            xref[35,k] = (xref[17,k+1] - xref[17,k])/dt_ft[4] # foot4 x vel
+            
+            xref[28,k] = (xref[10,k+1] - xref[10,k])/dt_ft[1] # foot1 z vel
+            xref[31,k] = (xref[13,k+1] - xref[13,k])/dt_ft[2] # foot2 z vel
+            xref[34,k] = (xref[16,k+1] - xref[16,k])/dt_ft[3] # foot3 z vel
+            xref[37,k] = (xref[19,k+1] - xref[19,k])/dt_ft[4] # foot4 z vel
         end
     end
     
@@ -70,7 +100,7 @@ function reference_trajectory(model::SimpleQuadruped, times, modes, terrain::Ter
     # body pos
     xref[1,N] = xterm
     xref[2,N] = 0
-    xref[3,N] = height
+    xref[3,N] = body_height
     
     xref[4,N] = 1 # no rotation
     xref[5,N] = 0 # 5:7 already set to 0, so this is unnecessary
